358dffb27dac095135a03e9e36bd593b307dfba1
ShimaYahya <sh.2742002@gmail.com>
2025-06-13 12:52:27 +0300

    upload backend 1

--- controllers/citiesController.js
+++ controllers/citiesController.js
@@ -1,24 +1,54 @@
-var models = require('../models');
+// var models = require('../models');
 
 
-// SHOW ALL 
+// // SHOW ALL 
+// exports.index = function (req, res, next) {
+//     var response = {
+//         success: false,
+//         message: [],
+//         data: {}
+//     }
+//     models.Cities.findAll({})
+//         .then(cities => {
+//             if (Array.isArray(cities)) {
+//                 response.data = cities
+//                 response.success = true
+//             } else {
+//                 response.message.push("Somthing wrong happend, try agien")
+//             }
+//         }).finally(() => {
+//             res.send(response)
+//         })
+// }
+
+// // done
+
+
+
+
+
+
 exports.index = function (req, res, next) {
-    var response = {
+    const response = {
         success: false,
         message: [],
         data: {}
-    }
-    models.Cities.findAll({})
-        .then(cities => {
-            if (Array.isArray(cities)) {
-                response.data = cities
-                response.success = true
-            } else {
-                response.message.push("Somthing wrong happend, try agien")
-            }
-        }).finally(() => {
-            res.send(response)
-        })
-}
+    };
 
-// done
+    models.Cities.findAll()
+        .then(cities => {
+            if (Array.isArray(cities) && cities.length > 0) {
+                response.data = cities;
+                response.success = true;
+            } else {
+                response.message.push("No cities found.");
+            }
+        })
+        .catch(err => {
+            console.error("❌ Error fetching cities:", err);
+            response.message.push("Database error occurred");
+        })
+        .finally(() => {
+            res.send(response);
+        });
+};
