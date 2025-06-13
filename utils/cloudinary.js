e255127b741cb5cf5c9ef7b1412390b70f06daf5
ShimaYahya <sh.2742002@gmail.com>
2025-06-13 15:38:31 +0300

    EDIT PHOTO 2222

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,28 @@
+// const cloudinary = require('cloudinary').v2;
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET
+// });
+
+// module.exports = cloudinary;
+
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = cloudinary;
+const cloudinaryUpload = async (filePath) => {
+  return await cloudinary.uploader.upload(filePath, {
+    folder: "projects"
+  });
+};
+
+module.exports = {
+  cloudinaryUpload
+};
+
