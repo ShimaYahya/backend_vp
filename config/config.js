7d1586eb9ca054847929e61f85417756cc1ce37c
ShimaYahya <sh.2742002@gmail.com>
2025-06-13 13:18:59 +0300

    aaa bbb ccc

--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
+// 
 
-console.log("🚀 DB_HOST is:", process.env.DB_HOST);
+
+
+require('dotenv').config();
 
 module.exports = {
   development: {
@@ -17,20 +19,18 @@ module.exports = {
       }
     }
   },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres'
-  },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
-};
+}
